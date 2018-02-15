6b44b245d04fdfdb4584f4a5ce2224599f838772
vpomo <pomogalov@gmail.com>
2018-02-15 20:07:20 +0300

    init

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+const ASTCrowdsale = artifacts.require('./ASTCrowdsale.sol');
+
+module.exports = (deployer) => {
+    //http://www.onlineconversion.com/unix_time.htm
+    var owner = "0x305EE1AEA4cE97F764618F0540EA6Fb37C7186Ba";
+    deployer.deploy(ASTCrowdsale, owner);
+
+};
